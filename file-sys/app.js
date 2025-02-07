9b9652a7fbaab5cc6b4da0634fa126c3b814128e
RajeshSR0809 <srrajesh.svpur@gmail.com>
2025-02-07 22:01:17 +0530

    adding commands to delete , rename, add content

--- file-sys/app.js
+++ file-sys/app.js
@@ -1,10 +1,14 @@
-import { watch, open } from "node:fs/promises";
+import { watch, open, unlink, rename, appendFile } from "node:fs/promises";
 
 
 
 (async () => {
     const COMMANDS = {
         CREATE_A_FILE: "create a file",
+        DELETE_A_FILE: "delete a file",
+        RENAME_A_FILE: "rename the file",
+        ADD_TO_FILE: "add to the"
+
     }
     
     const openFileHandelr = await open("./command.txt", "r");
@@ -18,7 +22,6 @@ import { watch, open } from "node:fs/promises";
 
     let createFile = async (command) => {
         let filePath = command.substring(COMMANDS.CREATE_A_FILE.length + 1);
-        console.log(filePath);
         let doesExists
         try {
             doesExists = await open(filePath, "r")
@@ -30,6 +33,41 @@ import { watch, open } from "node:fs/promises";
         }
     }
 
+    let deleteFile = async (command) => {
+        try {
+            let filePath = command.substring(COMMANDS.DELETE_A_FILE.length + 1);
+            await unlink(filePath)
+        } catch (error) {
+            console.error(`${filePath}, delete`)            
+        }
+
+    }
+
+    let renameFile = async (command) => {
+        try {
+            let idx = command.indexOf(" to ");
+            let oldFilePath = command.substring(COMMANDS.RENAME_A_FILE.length + 1, idx );
+            let newFilePath = command.substring(idx + 4 );
+            await rename(oldFilePath, newFilePath)
+        } catch (error) {
+            console.error(error);            
+        }
+    }
+
+    let addToFile = async (command) => {
+        try {
+            let idx = command.indexOf(" this content: ");
+            let filePath = command.substring(COMMANDS.ADD_TO_FILE.length + 1, idx)
+            let content = command.substring(idx + 15);
+    
+            console.log(filePath, content)
+            await appendFile(filePath, Buffer.from(content), { "encoding": "utf-8" })
+                
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
 
     openFileHandelr.on("change", async () => {
         let stats = await openFileHandelr.stat();
@@ -38,6 +76,15 @@ import { watch, open } from "node:fs/promises";
 
         if(command.includes(COMMANDS.CREATE_A_FILE)){
             createFile(command);
+        }
+        else if(command.includes(COMMANDS.DELETE_A_FILE)){
+            deleteFile(command)
+        }
+        else if(command.includes(COMMANDS.RENAME_A_FILE)){
+            renameFile(command);
+        }
+        else if(command.includes(COMMANDS.ADD_TO_FILE)){
+            addToFile(command)
         }
     });
 
---
+++ file-sys/readme.md
@@ -0,0 +1,4 @@
+create a file abc.js
+delete a file abc.js
+rename the file xyz.js to abc.js
+add to the file abc.js this content: javascript
